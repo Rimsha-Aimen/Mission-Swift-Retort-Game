8941750e0a93afa1c0613a30f4e72a2f9c51f9ed
unityprojectG <61115498+unityprojectG@users.noreply.github.com>
2020-03-24 12:33:28 +0500

    Create enemyhealth.cs
    comit

---
+++ Scripts/enemyhealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class enemyhealth : MonoBehaviour
+{
+
+    public Slider healthbar;
+    float health = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthbar.value = health;
+        if (health <= 0)
+        {
+            health = 0;
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision co)
+    {
+        if (co.gameObject.tag == "Player")
+        {
+            health -= 30;
+        }
+    }
+}
