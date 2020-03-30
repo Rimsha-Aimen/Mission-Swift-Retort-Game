19c330589694744e126120a743c8137a4f144bd3
unityprojectG <61115498+unityprojectG@users.noreply.github.com>
2020-03-30 11:10:38 +0500

    Create playerhealth.cs
    commit to master

---
+++ Scripts/playerhealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playrscript : MonoBehaviour
+{
+ 
+
+     public Slider healthbar;
+     float health = 100;
+     // Start is called before the first frame update
+     void Start()
+     {
+
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+         healthbar.value = health;
+         if(health <= 0)
+         {
+             health = 0;
+             Destroy(gameObject);
+         }
+     }
+
+     private void OnCollisionEnter(Collision co)
+     {
+         if(co.gameObject.tag=="Enemy")
+         {
+             health -= 30;
+         }
+     }
+ }
+    
+
